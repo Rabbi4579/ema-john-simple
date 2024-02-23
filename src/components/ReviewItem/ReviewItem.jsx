94a8213c0c193e4d2290b2c7bca473d00365aa4f
nooralamsiddique <samiketislam11@gmail.com>
2024-02-23 13:43:31 +0600

    added cart-review

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './ReviewItem.css'
-const ReviewItem = ({product}) => {
-    const {id, img, price, quantity} = product;
-    console.log(product)
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+const ReviewItem = ({product, handleRemoveFromCart}) => {
+    // console.log(product)
+    const {id, img, price, quantity, name} = product;
+    // console.log(removeCart)
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='review-title'>{name}</p>
+                <p>Price: <span className='orange-text'>{price}</span></p>
+                <p>Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete' onClick={() => handleRemoveFromCart(id)}>
+                <FontAwesomeIcon icon={faTrashAlt}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
